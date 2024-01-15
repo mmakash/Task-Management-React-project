55b9509043c689501c50e2fe9a21566c132ce38e
akash <musaddekurakash@gmail.com>
2024-01-15 22:00:12 +0600

    taskboard added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import HeroSection from './HeroSection';
+import TaskBoard from './Task/TaskBoard';
 
 
 const App = () => {
   return (
     <div>
      <Header />
-     <HeroSection />  
+    <div className='flex flex-col justify-center items-center'>
+    <HeroSection />  
+     <TaskBoard />
+    </div>
      <Footer />
     </div>
   );
